d79ffcfa47245f5d5895a0832788d0c422c72c84
mendes <mendeskendes@gmail.com>
2024-11-04 21:02:42 -0300

    tela inicial

--- concessionaria/script/login-cadastro.js
+++ concessionaria/script/login-cadastro.js
@@ -1,6 +1,8 @@
 const signUpButton = document.getElementById('signUp');
 const signInButton = document.getElementById('signIn');
 const container = document.getElementById('container');
+const entrarBtn = document.getElementById('btn-entrar');
+const criarBtn = document.getElementById('btn-criar-conta');
 
 signUpButton.addEventListener('click', () => {
 	container.classList.add("right-panel-active");
