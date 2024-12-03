4e477f0f813380980d430efbe1172dceb93cabd5
Icaro2M <icaromolina433@gmail.com>
2024-12-03 01:22:07 -0300

    Update PropostasAdm.js

--- proj/views/script/PropostasAdm.js
+++ proj/views/script/PropostasAdm.js
@@ -1,21 +1,4 @@
-async function getuserid(id) {
-    try {
-        const response = await fetch('http://localhost:3000/getuserbyid', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({id})
-        });
-        
-        const result = await response.json();
-        
-        return result
-        
-    } catch (error) {
-        console.error('Erro ao buscar:', error);
-    }
-}
+
 
 async function getveiculoid(id) {
     try {
@@ -94,17 +77,12 @@ async function getPropostas() {
 
 
 async function carregar(){
-    const userId = sessionStorage.getItem("userId")
-    if(userId){
-    let usuario = await getuserid(userId)
-    usuario = usuario[0]
+    
 
-    document.getElementById('perfilusuario').textContent = `Bem-vindo, ${usuario.nome}`;
-        document.getElementById('perfilusuario').classList.remove('user');
-        document.getElementById('perfilusuario').style.color = 'black';
+    
         let propostas = await getPropostas()
         carregarLista(propostas)
-    }
+    
 }
 
 carregar()
@@ -142,6 +120,7 @@ async function criarItem(proposta){
 
     var img = document.createElement("img")
     img.className = "proposta-imagem"
+    img.src = veiculo.imagem
 
     var div2 = document.createElement("div")
     div2.className = "proposta-detalhes"
