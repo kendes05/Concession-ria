8d3d4a2b5856131fefa3b7f47ee8bb48c0384f03
Icaro2M <icaromolina433@gmail.com>
2024-12-03 01:13:33 -0300

    Update Veiculo.js

--- proj/models/Veiculo.js
+++ proj/models/Veiculo.js
@@ -27,50 +27,10 @@ class VeiculoModel{
     }
 
 
-    static async getImagem(id){
+
+    static async addVeiculo(modelo, marca, ano, preco, cor,imagem){
         return new Promise((resolve, reject) => {
-            db.query('SELECT url FROM imagem where veiculo_idveiculo=? limit 1', [id], (error, result) => {
-                if (error) {
-                    reject(error);  // Adicionando reject para tratar o erro
-                } else {
-                    resolve(result);
-                }
-            });
-        });
-
-
-    }
-
-    static async getFotos(id){
-        return new Promise((resolve, reject) => {
-            db.query('SELECT url FROM imagem where veiculo_idveiculo=?', [id], (error, result) => {
-                if (error) {
-                    reject(error);  // Adicionando reject para tratar o erro
-                } else {
-                    resolve(result);
-                }
-            });
-        });
-
-
-    }
-
-
-    static async getMarca(id){
-        return new Promise((resolve, reject) => {
-            db.query('SELECT * FROM marca where idmarca=?', [id], (error, result) => {
-                if (error) {
-                    reject(error);  // Adicionando reject para tratar o erro
-                } else {
-                    resolve(result);
-                }
-            });
-        });
-    }
-
-    static async addveiculo(modelo,marca_idmarca,ano,preco,cor,tipo){
-        return new Promise((resolve, reject) => {
-            db.query('insert into veiculo(modelo,marca_idmarca,ano,preco,cor,tipo) values (?,?,?,?,?,?)', [modelo,marca_idmarca,ano,preco,cor,tipo], (error, result) => {
+            db.query('insert into veiculo(modelo,marca,ano,preco,cor) values (?,?,?,?,?,?)', [modelo, marca, ano, preco, cor,imagem], (error, result) => {
                 if (error) {
                     return reject(error);
                 }
@@ -90,18 +50,7 @@ class VeiculoModel{
         });
     }
     
-    static async addImg(url,idveiculo){
-        
-        return new Promise((resolve, reject) => {
-            db.query('insert into imagem (url, veiculo_idveiculo) values (?,?)', [url,idveiculo], (error, result) => {
-                if (error) {
-                    reject(error);  
-                } else {
-                    resolve(result);
-                }
-            });
-        });
-    }
+    
 
 
 }
